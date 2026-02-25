7165ee6532b8a779fab78ddfc3c9912b61841e88
avadhootkekan47-cyber <avadhootkekan47@gmail.com>
2026-02-25 11:26:54 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://rutbzscuufwldwmiobcb.supabase.co"
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1dGJ6c2N1dWZ3bGR3bWlvYmNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2OTU1OTksImV4cCI6MjA4NzI3MTU5OX0.jvy-oMaAf5Bl7M99IxsXE1npDdehL__xnmvl433tuwc"
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+```
